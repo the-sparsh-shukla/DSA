616bf330a99d1c1d150bcdf2368e3776a9c2ae13
Sparsh Shukla <sparsh.shukla@zohomail.in>
2026-04-11 18:39:32 +0530

    isValid method

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -5,14 +5,14 @@ class Solution {
         Stack<Character> st = new Stack<>();
 
         for (char ch : s.toCharArray()) {
-            // Push opening brackets
+            
             if (ch == '(' || ch == '{' || ch == '[') {
                 st.push(ch);
             } else {
-                // If stack empty, invalid
+                
                 if (st.isEmpty()) return false;
 
-                // Check matching pairs
+                
                 if (ch == ')' && st.peek() != '(') return false;
                 if (ch == '}' && st.peek() != '{') return false;
                 if (ch == ']' && st.peek() != '[') return false;
