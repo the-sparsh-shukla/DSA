004a857de1182c0ecd8bc4abd624ade7ed714e35
Sparsh Shukla <sparsh.shukla@zohomail.in>
2026-04-12 21:21:02 +0530

    Time: 4 ms (40.13%), Space: 44.3 MB (74.65%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int max = 0;
+
+        for (String s : sentences) {
+            int count = 1; 
+
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == ' ') {
+                    count++;
+                }
+            }
+
+            max = Math.max(max, count);
+        }
+
+        return max;
+    }
+}
