2f38e0658f9239178a5eac988962c886ecf15071
Sparsh Shukla <sparsh.shukla@zohomail.in>
2026-04-17 22:39:47 +0530

    Time: 0 ms (100%), Space: 43 MB (37.02%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String interpret(String command) {
+      StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == 'G') {
+                result.append('G');
+            } else if (command.charAt(i) == '(') {
+                if (command.charAt(i + 1) == ')') {
+                    result.append('o');
+                    i++; 
+                } else {
+                    result.append("al");
+                    i += 3; 
+                }
+            }
+        }
+
+        return result.toString();  
+    }
+}
