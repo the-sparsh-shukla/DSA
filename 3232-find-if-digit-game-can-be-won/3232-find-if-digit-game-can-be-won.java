88623f9b3cd0bfa7b8f1e17bb87a10b2782d1495
Sparsh Shukla <sparsh.shukla@zohomail.in>
2026-08-13 11:23:51 +0530

    Time: 0 ms (100%), Space: 45.5 MB (57.25%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int singleDigitSum = 0;
+        int doubleDigitSum = 0;
+
+        for (int num : nums) {
+            if (num < 10) {
+                singleDigitSum += num;
+            } else {
+                doubleDigitSum += num;
+            }
+        }
+
+        return singleDigitSum != doubleDigitSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 10};
+
+       
+    }
+}
