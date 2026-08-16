2ab22f971244a8e73b9dd1936d063e08522cfd95
Sparsh Shukla <sparsh.shukla@zohomail.in>
2026-08-16 13:49:17 +0530

    Time: 3 ms (74.31%), Space: 47.7 MB (67.53%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> result = new ArrayList<>();
+
+        int i = num.length - 1;
+        int carry = k;
+
+        while (i >= 0 || carry > 0) {
+
+            if (i >= 0) {
+                carry += num[i];
+                i--;
+            }
+
+            result.add(carry % 10);
+            carry = carry / 10;
+        }
+
+        Collections.reverse(result);
+
+        return result;
+    }
+}
