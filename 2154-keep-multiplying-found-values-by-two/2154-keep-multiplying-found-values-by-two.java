1acba629946c0e5b3853ab38802054dfcdcd3312
Sparsh Shukla <sparsh.shukla@zohomail.in>
2026-09-09 00:58:09 +0530

    Time: 1 ms (95.23%), Space: 45.6 MB (83.16%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+
+        boolean found = true;
+
+        while (found) {
+
+            found = false;
+
+            for (int i = 0; i < nums.length; i++) {
+
+                if (nums[i] == original) {
+                    original = original * 2;
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        return original;
+    }
+}
